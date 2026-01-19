9a29e95a6bbadaa3b889e96ce23f9eb2b7b7e52b
Himanshu singh <716himanshusingh@gmail.com>
2026-01-19 21:51:21 +0530

    Add files via upload

---
+++ conditionalst.java
@@ -0,0 +1,18 @@
+
+    //if-else
+    public class conditionalst{
+  
+    public static void main(String[] args) {
+      
+        int n = 6;
+
+        if (n > 5) {
+            System.out.println("The number is greater than 5.");
+        } else {
+            System.out.println("The number is 5 or less.");
+        }
+    }
+}
+
+    
+
