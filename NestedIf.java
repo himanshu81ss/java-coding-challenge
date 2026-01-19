185b5d4756f3205e2ccd8eb4478141fbb2f8d115
Himanshu singh <716himanshusingh@gmail.com>
2026-01-19 22:04:39 +0530

    Add files via upload

---
+++ NestedIf.java
@@ -0,0 +1,23 @@
+
+//nested-if
+import java.util.*;
+public class NestedIf {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int w= sc.nextInt();
+      
+       
+      
+        if (a >= 18) {
+            if (w >= 50.0) {
+                System.out.println("You are eligible to donate blood.");
+            } else {
+                System.out.println("You must weigh at least 50 kilograms to donate blood.");
+            }
+        } else {
+            System.out.println("You must be at least 18 years old to donate blood.");
+        }
+    }
+}
