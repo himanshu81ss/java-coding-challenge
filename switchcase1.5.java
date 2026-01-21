bb798651aa6da1e6773d9342eecde79e7dc2fa1b
Himanshu singh <716himanshusingh@gmail.com>
2026-01-21 22:18:46 +0530

    Add files via upload

---
+++ switchcase1.5.java
@@ -0,0 +1,18 @@
+//switchcase
+public class switchcase5{
+
+    public static void main(String[] args) {
+        Integer age = 25;
+
+        switch (age) { 
+            case 25:
+                System.out.println("You are 25.");
+                break;
+            case 30:
+                System.out.println("You are 30.");
+                break;
+            default:
+                System.out.println("Age not matched.");
+        }
+    }
+}
