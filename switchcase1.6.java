be3b8e37c0d7b8bd42db8b708912c9145bd919ad
Himanshu singh <716himanshusingh@gmail.com>
2026-01-21 22:19:04 +0530

    Add files via upload

---
+++ switchcase1.6.java
@@ -0,0 +1,17 @@
+//switchcasealphbet
+public class switchcase6 {
+    public static void main(String[] args) {
+        Character ch = 'c';
+
+        switch (ch) { 
+            case 'a':
+                System.out.println("You are a.");
+                break;
+            case 'c':
+                System.out.println("You are c.");
+                break;
+            default:
+                System.out.println("Character not matched.");
+        }
+    }
+}
