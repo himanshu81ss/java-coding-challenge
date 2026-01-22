660f6fc28e524984b10fcae11bce0304ba692fc1
Himanshu singh <716himanshusingh@gmail.com>
2026-01-22 13:55:20 +0530

    Add files via upload

---
+++ forloop1.1java.java
@@ -0,0 +1,15 @@
+//forloop1
+import java.util.*;
+public class forloop1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a =sc.nextInt();
+        int i;
+        for(i=a;i<10;i=i+1){
+            System.out.print(i);
+
+        }
+    }
+ 
+    
+}
