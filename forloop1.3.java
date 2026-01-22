438c09e8901e15bb6c8f53d395215e87e51c0fd9
Himanshu singh <716himanshusingh@gmail.com>
2026-01-22 13:56:42 +0530

    Add files via upload

---
+++ forloop1.3.java
@@ -0,0 +1,13 @@
+
+//forloop3
+import java.util.*;
+public class forloop3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int a;
+        for(a=0;a<=N;a=a+1){
+            System.out.println("My Name is Himanshu Singh");
+        }
+    }
+}
