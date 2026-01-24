1e4bbf6f2a202da2aaa9f9413a596dd0b27c69ce
Himanshu singh <716himanshusingh@gmail.com>
2026-01-24 21:42:28 +0530

    Add files via upload

---
+++ pattern1.4.java
@@ -0,0 +1,15 @@
+//pattern4
+public class pattern4 {
+	public static void main(String[] args)
+	{
+		int N = 5;
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N-1 ; j++) {
+				System.out.print("*");
+			}
+			
+			
+			System.out.println();
+		}
+	}
+}
