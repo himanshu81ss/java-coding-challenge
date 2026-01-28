884b898abd26fefa0d2ba4aa1603d1c47d98f929
Himanshu singh <716himanshusingh@gmail.com>
2026-01-28 20:56:57 +0530

    Add files via upload

---
+++ pattern1.7.java
@@ -0,0 +1,20 @@
+//patern7
+import java.util.*;
+public class pattern7 {
+    public static int calculateSum(int n){
+        int sum=0;
+        int i;
+        for(i=0;i<=n;i++){
+            if(i%2!=0){
+                sum=sum+i;
+            }   
+        }
+        return sum;
+    }
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int sum=calculateSum(n);
+    System.out.print(sum);
+}
+}
